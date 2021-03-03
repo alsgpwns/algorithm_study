44cd547e634758240d0f2fdc6484435064ff9510
Sung Jun Jin <sungjun@peoplefund.co.kr>
2021-03-03 22:23:15 +0900

    feat: day5 문자열을 정수로 바꾸기 풀이

--- sungjun/day5.py
+++ sungjun/day5.py
@@ -93,5 +93,22 @@ def char_to_upper(s):
                 word[idx] = word[idx].lower()
 
         answer.append("".join(word))
-        
+
     return " ".join(answer)
+
+
+def is_instance(s):
+    """
+    문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+
+    [ 제한 조건 ]
+    s의 길이는 1 이상 5이하입니다.
+    s의 맨앞에는 부호(+, -)가 올 수 있습니다.
+    s는 부호와 숫자로만 이루어져있습니다.
+    s는 "0"으로 시작하지 않습니다.
+    """
+    if isinstance(s, str):
+        return int(s)
+    elif isistance(s, int):
+        return str(s)
+
