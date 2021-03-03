5a76f4bd66d9a2f568b43aa2640630aeeafae6c2
Sung Jun Jin <sungjun@peoplefund.co.kr>
2021-03-03 22:24:54 +0900

    feat: day5 평균 구하기 풀이

--- sungjun/day5.py
+++ sungjun/day5.py
@@ -112,3 +112,10 @@ def is_instance(s):
     elif isistance(s, int):
         return str(s)
 
+
+def get_average(arr):
+    """
+    정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+    """
+    return sum(arr) / len(arr)
+
