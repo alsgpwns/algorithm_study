b2ff566bcd879b4df2317441c70671326066e8d9
Sung Jun Jin <sungjun@peoplefund.co.kr>
2021-03-03 22:17:47 +0900

    feat: day5 이상한 문자열 만들기 풀이

--- sungjun/day5.py
+++ sungjun/day5.py
@@ -71,3 +71,27 @@ def exhaustive_search():
 
     return student_index_list
 
+
+def char_to_upper(s):
+    """ 문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다. 각 단어의 짝수번째 알파벳은 대문자로,
+        홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+
+        [제한 사항]
+        문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
+        첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
+    """
+    words = s.split(" ")
+    answer = []
+
+    for word in words:
+        word = list(word)
+
+        for idx, char in enumerate(word):
+            if idx % 2 == 0:
+                word[idx] = word[idx].upper()
+            else:
+                word[idx] = word[idx].lower()
+
+        answer.append("".join(word))
+        
+    return " ".join(answer)
